4a15c6d70f365d30480d080635bc0305300b389a
OUAHI MOHAMED <ouahi49@gmail.com>
2025-08-28 15:50:24 +0000

    Fix import and update routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ import { Unauthorized } from './pages/Unauthorized';
 import { RequestLeave } from './pages/RequestLeave';
 import { Profile } from './pages/Profile';
 import { Responsables } from './pages/Responsables';
-
 import { ToastProvider } from './components/ui/use-toast';
-import { ProfileAdmin } from './pages/ProfileAdmin';
+import { ProfileAdmin } from './pages/ProfileAdmin ';
 import Login from './pages/Login';
 import { MarkAbsence } from './pages/MarkAbsence';
 import { EmployeeAbsences } from './pages/EmployeeAbsences';
 import KitchenManagement from './pages/KitchenManagement';
+import SuiviVoiture from './pages/SuiviVoiture';
  function App() {
   return (
     <ToastProvider>
@@ -30,7 +30,7 @@ import KitchenManagement from './pages/KitchenManagement';
             <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
             <Route path="/employees" element={<ProtectedRoute requiredRole={['super_admin', 'responsable']}><Employees /></ProtectedRoute>} />
             <Route path="/leaves" element={<ProtectedRoute><Leaves /></ProtectedRoute>} />
-            <Route path="/request-leave" element={<ProtectedRoute requiredRole={['employe','responsable']}><RequestLeave /></ProtectedRoute>} />
+            <Route path="/request-leave" element={<ProtectedRoute requiredRole={['employe', 'responsable']}><RequestLeave /></ProtectedRoute>} />
             <Route path="/profile" element={<ProtectedRoute requiredRole={['employe','responsable']}><Profile /></ProtectedRoute>} />
             <Route path="/profiladmin" element={<ProtectedRoute requiredRole={['super_admin' ]}><ProfileAdmin /></ProtectedRoute>} />
             <Route path="/calendar" element={<ProtectedRoute><CalendarPage /></ProtectedRoute>} />
@@ -39,7 +39,9 @@ import KitchenManagement from './pages/KitchenManagement';
             <Route path="/notifications" element={<ProtectedRoute><Notifications /></ProtectedRoute>} />
             <Route path="/responsables" element={<ProtectedRoute><Responsables /></ProtectedRoute>} />
             <Route path="/absences" element={<ProtectedRoute><MarkAbsence /></ProtectedRoute>} />
-            <Route path="/justification" element={<ProtectedRoute><EmployeeAbsences /></ProtectedRoute>} />
+            <Route path="/cars" element={<ProtectedRoute><SuiviVoiture  /></ProtectedRoute>} />
+
+            <Route path="/justification" element={<ProtectedRoute requiredRole={['super_admin', 'responsable']}><EmployeeAbsences /></ProtectedRoute>} />
             <Route path="/unauthorized" element={<Unauthorized />} />
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
             <Route path="/kitchen" element={<ProtectedRoute requiredRole={['super_admin', 'responsable','employe']}><KitchenManagement user={undefined}  /></ProtectedRoute>} />
