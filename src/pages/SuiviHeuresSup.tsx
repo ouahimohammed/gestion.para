0ac6be179308cb392c9285dd20f444af6df1e57a
OUAHI MOHAMED <ouahi49@gmail.com>
2025-08-29 12:37:30 +0100

    Fix loading div gradient background

--- src/pages/SuiviHeuresSup.tsx
+++ src/pages/SuiviHeuresSup.tsx
@@ -470,7 +470,7 @@ const SuiviHeuresSup = () => {
 
   if (loading) {
     return (
-      <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 flex items-center justify-center">
+      <div className="min-h-screen bg-gradient-to-br   flex items-center justify-center">
         <div className="text-center">
           <div className="relative">
             <div className="animate-spin rounded-full h-16 w-16 border-4 border-blue-200 border-t-blue-600 mx-auto mb-6"></div>
